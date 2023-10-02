8cb42599f0325c6217ddbd7f624aa954deafb74d
tuanminh96 <pmt.bka.96@gmail.com>
2023-10-02 17:25:45 +0700

    recommit as reviewer

---
+++ backend/setup.bat
@@ -0,0 +1,3 @@
+set AUTH0_DOMAIN = dev-4yewlodtqwpxz0xf.us.auth0.com
+set ALGORITHMS = RS256
+set API_AUDIENCE =capstone
---
+++ backend/setup.sh
@@ -0,0 +1,3 @@
+export AUTH0_DOMAIN = dev-4yewlodtqwpxz0xf.us.auth0.com
+export ALGORITHMS = RS256
+export API_AUDIENCE = capstone
