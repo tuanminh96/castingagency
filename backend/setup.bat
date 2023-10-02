a4e7ab0dcbac7a7382679342036778406d27ba42
tuanminh96 <pmt.bka.96@gmail.com>
2023-10-02 21:01:59 +0700

    fix code as reviewer

--- backend/setup.bat
+++ backend/setup.bat
@@ -1,3 +1,3 @@
-set AUTH0_DOMAIN = dev-4yewlodtqwpxz0xf.us.auth0.com
-set ALGORITHMS = RS256
-set API_AUDIENCE =capstone
+set AUTH0_DOMAIN='dev-4yewlodtqwpxz0xf.us.auth0.com'
+set ALGORITHMS='RS256'
+set API_AUDIENCE='capstone'
